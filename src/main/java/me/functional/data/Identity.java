2290ad658820dc5ea85a66715653353f19662388
drjoliv <drjoliv@gmail.com>
2018-03-01 23:07:40 -0500

    initial framework for monads

---
+++ src/main/java/me/functional/data/Identity.java
@@ -0,0 +1,48 @@
+package me.functional.data;
+
+import java.util.function.Function;
+
+import me.functional.hkt.Hkt;
+import me.functional.hkt.Witness;
+
+public class Identity<E> implements Monad<Identity.μ,E>, Hkt<Identity.μ,E> {
+
+  public static <E> Identity<E> of(E e) {
+    return new Identity<E>(e);
+  }
+
+  public static <E> Identity<E> narrow(Monad<Identity.μ,E> e) {
+    return (Identity<E>) e;
+  }
+
+  @Override
+  public <B> Identity<B> mBind(Function<E, ? extends Monad<Identity.μ, B>> fn) {
+    return narrow(fn.apply(e));
+  }
+
+  @Override
+  public <B> Identity<B> mBind(Monad<Identity.μ, B> mIdentity) {
+    return mBind(e -> mIdentity);
+  }
+
+  @Override
+  public <B> Monad<Identity.μ, B> unit(B b) {
+    return of(b);
+  }
+
+  public E value() {
+    return e;
+  }
+
+  public static class μ implements Witness{}
+
+  private final E e;
+
+  private Identity(E e) {
+    this.e = e;
+  }
+
+  public <A> Identity<A> fmap(Function<E,A> fn) {
+    return new Identity<A>(fn.apply(e));
+  }
+}
---
+++ src/main/java/me/functional/hkt/Witness.java
@@ -0,0 +1,8 @@
+package me.functional.hkt;
+
+/**
+ * Types that are witnesses should implement this inteferace.
+ *
+ * @author Desonte "drjolive" Jolivet
+ */
+public interface Witness {}
