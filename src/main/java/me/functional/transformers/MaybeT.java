2b94f2eb12f11f64b30c713257ab448d9a2a8100
drjoliv <drjoliv@gmail.com>
2018-04-16 13:45:19 -0400

    renaming and using F1 instead of Function

--- src/main/java/me/functional/transformers/MaybeT.java
+++ src/main/java/me/functional/transformers/MaybeT.java
@@ -3,35 +3,36 @@ package me.functional.transformers;
 import java.util.function.Function;
 
 import me.functional.data.Maybe;
+import me.functional.functions.F1;
 import me.functional.functions.F2;
 import me.functional.hkt.Hkt;
 import me.functional.hkt.Hkt2;
 import me.functional.hkt.Witness;
-import me.functional.type.Monad;
-import me.functional.type.MonadUnit;
+import me.functional.type.Bind;
+import me.functional.type.BindUnit;
 
-public class MaybeT<M extends Witness,A> implements Monad<Hkt<MaybeT.μ,M>,A>, Hkt2<MaybeT.μ,M,A> {
+public class MaybeT<M extends Witness,A> implements Bind<Hkt<MaybeT.μ,M>,A>, Hkt2<MaybeT.μ,M,A> {
 
   public static class μ implements Witness{}
 
-  private final Monad<M,Maybe<A>> runMaybeT;
-  private final MonadUnit<M> mUnit;
+  private final Bind<M,Maybe<A>> runMaybeT;
+  private final BindUnit<M> mUnit;
 
   @Override
-  public <B> MaybeT<M,B> fmap(Function<? super A, B> fn){
+  public <B> MaybeT<M,B> fmap(F1<? super A, B> fn){
     return maybeT(runMaybeT.fmap(maybe_value -> maybe_value.fmap(fn)));
   }
 
   @Override
-  public <B> MaybeT<M,B> semi(Monad<Hkt<μ, M>, B> mb) {
+  public <B> MaybeT<M,B> semi(Bind<Hkt<μ, M>, B> mb) {
     return mBind(a -> mb);
   }
 
   @Override
-  public <B> MaybeT<M,B> mBind(Function<? super A, ? extends Monad<Hkt<μ, M>, B>> fn) {
+  public <B> MaybeT<M,B> mBind(F1<? super A, ? extends Bind<Hkt<μ, M>, B>> fn) {
     return maybeT(runMaybeT.mBind(maybe_value -> {
      if(maybe_value.isSome()) {
-       return asMaybeT(fn.apply(maybe_value.value())).runMaybeT;
+       return asMaybeT(fn.call(maybe_value.value())).runMaybeT;
      }
      else
        return mUnit.unit(Maybe.nothing());
@@ -39,42 +40,42 @@ public class MaybeT<M extends Witness,A> implements Monad<Hkt<MaybeT.μ,M>,A>, H
   }
 
   @Override
-  public MonadUnit<Hkt<μ, M>> yield() {
-    return new MonadUnit<Hkt<μ, M>>() {
+  public BindUnit<Hkt<μ, M>> yield() {
+    return new BindUnit<Hkt<μ, M>>() {
       @Override
-      public <B> Monad<Hkt<μ, M>, B> unit(B b) {
+      public <B> Bind<Hkt<μ, M>, B> unit(B b) {
         return MaybeT.<M,B>maybeT().call(mUnit,b);
       }
     };
   }
 
 
-  private MaybeT(Monad<M,Maybe<A>> runMaybeT) {
+  private MaybeT(Bind<M,Maybe<A>> runMaybeT) {
     this.runMaybeT = runMaybeT;  
     this.mUnit = runMaybeT.yield();
   }
 
-  public static <M extends Witness,A> MaybeT<M,A> liftMaybeT(Monad<M,A> m) {
+  public static <M extends Witness,A> MaybeT<M,A> liftMaybeT(Bind<M,A> m) {
     return new MaybeT<M,A>(m.fmap(a -> Maybe.maybe(a)));
   }
 
-  public static <M extends Witness, A> F2<MonadUnit<M>,A,MaybeT<M,A>> maybeT() {
+  public static <M extends Witness, A> F2<BindUnit<M>,A,MaybeT<M,A>> maybeT() {
     return (mUnit, a) -> new MaybeT<M, A>(mUnit.unit(Maybe.maybe(a)));
   }
 
-  public static <M extends Witness,A> MaybeT<M,A> maybeT(Monad<M,Maybe<A>> runMaybeT) {
+  public static <M extends Witness,A> MaybeT<M,A> maybeT(Bind<M,Maybe<A>> runMaybeT) {
     return new MaybeT<M,A>(runMaybeT);
   }
 
-  public static <M extends Witness,A> MaybeT<M,A> maybeT(A a, MonadUnit<M> mUnit) {
+  public static <M extends Witness,A> MaybeT<M,A> maybeT(A a, BindUnit<M> mUnit) {
     return liftMaybeT(mUnit.unit(a));
   }
 
-  public Monad<M,Maybe<A>> runMaybeT() {
+  public Bind<M,Maybe<A>> runMaybeT() {
     return runMaybeT;
   }
 
-  public static <M extends Witness, A> MaybeT<M, A> asMaybeT(Monad<Hkt<MaybeT.μ, M>, A> wider) {
+  public static <M extends Witness, A> MaybeT<M, A> asMaybeT(Bind<Hkt<MaybeT.μ, M>, A> wider) {
     return (MaybeT<M, A>) wider;
   }
 }
--- src/main/java/me/functional/transformers/TryT.java
+++ src/main/java/me/functional/transformers/TryT.java
@@ -1,74 +1,65 @@
 package me.functional.transformers;
 
-import static me.functional.data.Try.failure;
+import static me.functional.data.Try.*;
 
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import me.functional.data.Either;
 import me.functional.data.Try;
+import me.functional.functions.F0;
+import me.functional.functions.F1;
 import me.functional.hkt.Hkt;
 import me.functional.hkt.Hkt2;
 import me.functional.hkt.Witness;
-import me.functional.type.Monad;
-import me.functional.type.MonadUnit;
-import static me.functional.data.Try.*;
+import me.functional.type.Bind;
+import me.functional.type.BindUnit;
 
-public class TryT<M extends Witness,A> implements Monad<Hkt<TryT.μ,M>, A>, Hkt2<TryT.μ,M,A> {
+/**
+ *
+ *
+ * @author
+ */
+public class TryT<M extends Witness,A> implements Bind<Hkt<TryT.μ,M>, A>, Hkt2<TryT.μ,M,A> {
 
   public static class μ implements Witness{}
 
-  private final Supplier<Monad<M,Try<A>>> runTryT;
-  private final MonadUnit<M> mUnit;
+  private final F0<Bind<M,Try<A>>> runTryT;
 
-  public Monad<M,Try<A>> runTryT() {
-    return runTryT.get();  
-  }
+  private final BindUnit<M> mUnit;
 
-  private TryT(Try<A> t, MonadUnit<M> mUnit) {
+  private TryT(Try<A> t, BindUnit<M> mUnit) {
     this.runTryT = () -> mUnit.unit(t);
     this.mUnit   = mUnit;
   }
 
-  private TryT(Monad<M,Try<A>> t) {
+  private TryT(Bind<M,Try<A>> t) {
     this.runTryT = () -> t;
     this.mUnit   = t.yield();
   }
 
-  private TryT(Supplier<Monad<M,Try<A>>> runTryT, MonadUnit<M> mUnit) {
+  private TryT(F0<Bind<M,Try<A>>> runTryT, BindUnit<M> mUnit) {
     this.runTryT = runTryT;
     this.mUnit   = mUnit;
   }
 
-  public static <M extends Witness, B> TryT<M,B> tryT(B b, MonadUnit<M> mUnit) {
-    return new TryT<>(success(b), mUnit);
-  }
-
-  public static <M extends Witness, B> TryT<M,B> tryT(Monad<M,Try<B>> t) {
-    return new TryT<>(t);
-  }
-
-  public static <M extends Witness, B> TryT<M,B> lift(Monad<M,B> t) {
-    return new TryT<M,B>(t.fmap(b -> success(b)));
+  @Override
+  public <B> TryT<M,B> fmap(F1<? super A, B> fn) {
+    return new TryT<>(() -> runTryT.call().fmap(t -> t.fmap(fn)), mUnit); 
   }
 
   @Override
-  public <B> TryT<M,B> fmap(Function<? super A, B> fn) {
-    return new TryT<>(() -> runTryT.get().fmap(t -> t.fmap(fn)), mUnit); 
-  }
-
-  @Override
-  public <B> TryT<M,B> mBind(Function<? super A, ? extends Monad<Hkt<μ, M>, B>> fn) {
+  public <B> TryT<M,B> mBind(F1<? super A, ? extends Bind<Hkt<μ, M>, B>> fn) {
     return new TryT<M,B>(() -> {
-     Monad<M,Try<B>> m = runTryT.get().mBind(t -> {
-       Either<Exception, Supplier<Monad<M,Try<B>>>> e =
-         t.fmap(fn.andThen(TryT::asTryT))
+     Bind<M,Try<B>> m = runTryT.call().mBind(t -> {
+       Either<Exception, F0<Bind<M,Try<B>>>> e =
+         t.fmap(fn.then(TryT::asTryT))
          .run()
          .right()
          .fmap(tryT -> tryT.runTryT)
          .either();
        if(e.isRight())
-         return e.valueR().get();
+         return e.valueR().call();
        else
          return mUnit.unit(failure(e.valueL()));
      });
@@ -77,21 +68,67 @@ public class TryT<M extends Witness,A> implements Monad<Hkt<TryT.μ,M>, A>, Hkt2
   }
 
   @Override
-  public <B> TryT<M,B> semi(Monad<Hkt<μ, M>, B> mb) {
+  public <B> TryT<M,B> semi(Bind<Hkt<μ, M>, B> mb) {
     return mBind(a -> mb);
   }
 
   @Override
-  public MonadUnit<Hkt<μ, M>> yield() {
-    return new MonadUnit<Hkt<μ, M>>() {
+  public BindUnit<Hkt<μ, M>> yield() {
+    return new BindUnit<Hkt<μ, M>>() {
       @Override
-      public <B> Monad<Hkt<μ, M>, B> unit(B b) {
+      public <B> Bind<Hkt<μ, M>, B> unit(B b) {
         return new TryT<M,B>(mUnit.unit(success(b)));
       }
     };
   }
 
-  public static <M extends Witness, B> TryT<M, B> asTryT(Monad<Hkt<μ, M>, B> monad) {
+  /**
+  *
+  *
+  * @return
+  */
+  public Bind<M,Try<A>> runTryT() {
+    return runTryT.call();
+  }
+
+  /**
+   *
+   *
+   * @param b
+   * @param mUnit
+   * @return
+   */
+  public static <M extends Witness, B> TryT<M,B> tryT(B b, BindUnit<M> mUnit) {
+    return new TryT<>(success(b), mUnit);
+  }
+
+  /**
+   *
+   *
+   * @param t
+   * @return
+   */
+  public static <M extends Witness, B> TryT<M,B> tryT(Bind<M,Try<B>> t) {
+    return new TryT<>(t);
+  }
+
+  /**
+   *
+   *
+   * @param t
+   * @return
+   */
+  public static <M extends Witness, B> TryT<M,B> lift(Bind<M,B> t) {
+    return new TryT<M,B>(t.fmap(b -> success(b)));
+  }
+
+  /**
+   *
+   *
+   * @param monad
+   * @return
+   */
+  public static <M extends Witness, B> TryT<M, B> asTryT(Bind<Hkt<μ, M>, B> monad) {
     return (TryT<M, B>) monad;
   }
 }
