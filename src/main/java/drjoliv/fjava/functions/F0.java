b415bbbbec109215ccb573e33ee72ecefb5f9c45
drjoliv <drjoliv@gmail.com>
2018-05-16 08:23:21 -0400

    made F0 functor instance

--- src/main/java/drjoliv/fjava/functions/F0.java
+++ src/main/java/drjoliv/fjava/functions/F0.java
@@ -1,14 +1,25 @@
-package me.functional.functions;
+package drjoliv.fjava.functions;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-public interface F0<A> extends Supplier<A> {
+import drjoliv.fjava.control.Functor;
+import drjoliv.fjava.data.FList;
+import drjoliv.fjava.hkt.Witness;
+
+public interface F0<A> extends Supplier<A>, Functor<F0.μ,A> {
+
+  public interface μ extends Witness{}
+
   public A call();
+
+  @Override
+  public default <B> F0<B> map(F1<? super A, B> fn) {
+    return fn.curry().call(call());
+  }
 
   @Override
   public default A get() {
     return call();
   }
-
 }
