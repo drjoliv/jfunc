1e1d63a0fc11a6cf80bdb1fed72ba79770457847
drjoliv <drjoliv@gmail.com>
2018-05-22 11:32:28 -0400

    converts functions into one that accpets a tuple

--- src/main/java/drjoliv/fjava/functions/F2.java
+++ src/main/java/drjoliv/fjava/functions/F2.java
@@ -2,6 +2,8 @@ package drjoliv.fjava.functions;
 
 import java.util.function.BiFunction;
 
+import drjoliv.fjava.data.T2;
+
 public interface F2<A,B,C> extends BiFunction<A,B,C> {
 
   public C call(A a, B b);
@@ -13,6 +15,10 @@ public interface F2<A,B,C> extends BiFunction<A,B,C> {
   @Override
   public default C apply(A a, B b) {
     return call(a,b);
+  }
+
+  public default F1<T2<A,B>, C> tuple() {
+    return t -> call(t._1,t._2);
   }
 
   public static <A,B,C> F1<B,C> partial(F2<A,B,C> fn2, A a) {
