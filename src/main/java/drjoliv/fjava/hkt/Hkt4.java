4e0cea5704331218a0efa7c37488224592093324
drjoliv <drjoliv@gmail.com>
2018-06-01 11:59:43 -0400

    added more higher kinded types

---
+++ src/main/java/drjoliv/fjava/hkt/Hkt4.java
@@ -0,0 +1,3 @@
+package drjoliv.fjava.hkt;
+
+public interface Hkt4<M extends Witness,a,b,c,d> extends Hkt<Hkt3<M,a,b,c>,d> {}
---
+++ src/main/java/drjoliv/fjava/hkt/Hkt5.java
@@ -0,0 +1,3 @@
+package drjoliv.fjava.hkt;
+
+public interface Hkt5 <M extends Witness,a,b,c,d,e> extends Hkt<Hkt4<M,a,b,c,d>,e> {}
