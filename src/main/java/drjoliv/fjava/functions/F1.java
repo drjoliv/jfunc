822c3235c9f1d2a60d577f8dde4bbc4856206339
drjoliv <drjoliv@gmail.com>
2018-06-24 13:52:47 -0400

    added javadocs

--- src/main/java/drjoliv/fjava/functions/F1.java
+++ src/main/java/drjoliv/fjava/functions/F1.java
@@ -1,49 +1,84 @@
 package drjoliv.fjava.functions;
 
-import static drjoliv.fjava.data.Dequeue.dequeue;
 import static drjoliv.fjava.functions.F1Composed.isComposedFunc;
 
 import java.util.function.Function;
 
-import drjoliv.fjava.control.Functor;
+import drjoliv.fjava.functor.Functor;
 import drjoliv.fjava.hkt.Hkt;
 import drjoliv.fjava.hkt.Witness;
+import drjoliv.fjava.adt.Dequeue;
+import static drjoliv.fjava.adt.Dequeue.*;
 
+/**
+ * A function from {@code A} to {@code B}.
+ * @author Desonte 'drjoliv' Jolivet : drjoliv@gmail.com
+ */
+@FunctionalInterface
 public interface F1<A,B> extends Functor<Hkt<F1.μ,A>,B>{
 
-  public class μ implements Witness{}
+  /**
+  * Teh witness type of {@code F1}.
+  */
+  public class μ implements Witness{private μ(){}}
 
+  /**
+   * Returns the result of this function.
+   * @param a the argument for this function.
+   * @return the result of this function.
+   */
   public B call(A a);
 
-  public default F1<A,F0<B>> curry() {
-    return a -> () -> call(a);
-  }
-
   @Override
-  public default <C> F1<A,C> map(F1< ? super B, C> fn) {
+  public default <C> F1<A,C> map(F1< ? super B, ? extends C> fn) {
     return then(fn);
   }
 
-  public default Function<A,B> toJFunc() {
-    return a -> call(a);
+  /**
+   * Transfroms this function from {@code fn::A -> B} to {@code fn::A -> F0<B>}.
+   * @return a {@code fn::A→ F0<B>}.
+   */
+  public default F1<A,F0<B>> curry() {
+    return a -> () -> {
+      return call(a);
+    };
   }
 
-
-  public default <C> F1<A,C> then(F1< ? super B, C> fn) {
-    return fn.before(this);
+  /**
+   * Creates a composition of this function and the argument such that this function is applied first.
+   * {@code g = this}
+   * {@code f = fn}
+   * {@code h = (f . g) }
+   * {@code h(a) == f(g(a)) }
+   *
+   * @param fn a function to compose.
+   * @return a composed function.
+   */
+  public default <C> F1<A,C> then(F1< ? super B, ? extends C> fn) {
+    return (F1<A,C>)fn.before(this);
   }
 
-  public default <C> F1<C,B> before(F1<C ,? extends A> fn) {
+  /**
+   * Creates a composition of this function and the argument such that argument function is applied first.
+   * {@code g = fn}
+   * {@code f = this}
+   * {@code h = (f . g) }
+   * {@code h(a) == f(g(a)) }
+   *
+   * @param fn a function to compose.
+   * @return a composed function.
+   */
+  public default <C> F1<C,B> before(F1<? super C ,? extends A> fn) {
     return isComposedFunc(fn)
-      ? fn.then(this)
+      ? (F1<C,B>)fn.then(this)
       : new F1Composed<C,B>(dequeue(fn,this));
   }
 
+  /**
+   * Returns the identity function.
+   * @return the identity function.
+   */
   public static <A> F1<A, A> identity() {
     return a -> a;
-  }
-
-  public static <A, B> F1<A, B> fromJFunction(Function<A, B> fn) {
-    return a -> fn.apply(a);
   }
 }
--- src/main/java/drjoliv/fjava/functions/F6.java
+++ src/main/java/drjoliv/fjava/functions/F6.java
@@ -1,49 +1,118 @@
 package drjoliv.fjava.functions;
 
-import drjoliv.fjava.control.Functor;
-import drjoliv.fjava.data.T6;
+import drjoliv.fjava.functor.Functor;
 import drjoliv.fjava.hkt.Hkt6;
 import drjoliv.fjava.hkt.Witness;
+import drjoliv.fjava.hlist.T6;
 
+/**
+ *
+ * A computation that takes six arguments and returns a result.
+ * @author Desonte 'drjoliv' Jolivet : drjoliv@gmail.com
+ */
+@FunctionalInterface
 public interface F6<A,B,C,D,E,F,G> extends Functor<Hkt6<F6.μ,A,B,C,D,E,F>,G>{
 
-  public G call(A a, B b, C c, D d, E e, F f);
-
+  /**
+  * The witness type of {@code F6}
+  */
   public static class μ implements Witness{}
 
   @Override
-  public default <H> F6<A,B,C,D,E,F,H> map(F1<? super G, H> fn) {
+  public default <H> F6<A,B,C,D,E,F,H> map(F1<? super G, ? extends H> fn) {
     return F6Composed.doMap(this,fn);
   }
 
+  /**
+   * Returns the result of this computation.
+   * @param a an argument.
+   * @param b an argument.
+   * @param c an argument.
+   * @param d an argument.
+   * @param e an argument.
+   * @param f an argument.
+   * @return the result of this computation.
+   */
+  public G call(A a, B b, C c, D d, E e, F f);
+
+  /**
+   * Partially applies five arguments, returning a function that takes one argument.
+   * @param a an argument.
+   * @param b an argument.
+   * @param c an argument.
+   * @param d an argument.
+   * @param e an argument.
+   * @return a partially applied function that takes one argumennt.
+   */
   public default F1<F,G> call(A a, B b, C c, D d, E e) {
     return call(a).call(b).call(c).call(d).call(e);
   }
 
+  /**
+   *
+   * Partially applies four arguments, returning a function that takes two arguments.
+   * @param a an argmuent.
+   * @param a an argument.
+   * @param c an argument.
+   * @param d an argument.
+   * @return a partially applied function that takes two arguments.
+   */
   public default F2<E,F,G> call(A a, B b, C c, D d) {
     return call(a).call(b).call(c).call(d);
   }
 
+  /**
+   * Partially applies three arguments, returning a function that takes three arguments.
+   * @param a an argument.
+   * @param b an argument.
+   * @param c an argument.
+   * @return a partially applied function that takes three arguments.
+   */
   public default F3<D,E,F,G> call(A a, B b, C c) {
     return call(a).call(b).call(c);
   }
 
+   /**
+   * Paritally applies the two arguments, returning a function that takes four arguments.
+   * @param a an argument.
+   * @param b an argument.
+   * @return a partially applied function that takes four arguments.
+   */
   public default F4<C,D,E,F,G> call(A a, B b) {
     return call(a).call(b);
   }
 
+  /**
+   * Paritally applies an argument, returning a function that takes five arguments.
+   * @param an argument.
+   * @return a function that takes five arguments.
+   */
   public default F5<B,C,D,E,F,G> call(A a) {
     return partial(this,a);
   }
 
+  /**
+   * Converts this function into one that returns a functions.
+   * @return a function that returns a function of arity 5.
+   */
   public default F1<A,F5<B,C,D,E,F,G>> curry() {
     return a -> partial(this,a);
   }
 
+  /**
+   * Converts this function into one that takes a single argument of {@code T3<A,B,C,D,E,F>}.
+   * @return a function that take a single arguemnt of {@code T2<A,B,C,D,E,F>} and returns a result.
+   */
   public default F1<T6<A,B,C,D,E,F>,G> t6() {
     return t -> call(t._1(),t._2(),t._3(), t._4(), t._5(), t._6());
   }
 
+   /**
+   * Partially appiles the value {@code a} to the function {@code fn}, returning a partially applied function of arity 5.
+   * @param fn a funcion to be partially applied.
+   * @param a a argument to be applied.
+   * @return a partially applied function of arity 5.
+   */
   public static <A,B,C,D,E,F,G> F5<B,C,D,E,F,G> partial(F6<A,B,C,D,E,F,G> fn6, A a) {
     return (b,c,d,e,f) -> fn6.call(a,b,c,d,e,f);
   }
